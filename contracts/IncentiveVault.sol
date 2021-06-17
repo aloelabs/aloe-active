858c3090d09c6dd9d123b818593db89c2025de0e
hayden Shively <haydenshively@gmail.com>
2021-06-17 04:39:15 -0500

    Add advance incentive

--- contracts/IncentiveVault.sol
+++ contracts/IncentiveVault.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract StakingIncentiveVault {
+contract IncentiveVault {
     address immutable multisig;
 
     /// @dev A mapping from predictions address to token address to incentive per epoch (amount)
-    mapping(address => mapping(address => uint256)) public incentivesPerEpoch;
+    mapping(address => mapping(address => uint256)) public stakingIncentivesPerEpoch;
+
+    /// @dev A mapping from predictions address to token address to incentive per advance (amount)
+    mapping(address => mapping(address => uint256)) public advanceIncentives;
 
     mapping(bytes32 => bool) public claimed;
 
@@ -46,22 +49,22 @@ contract StakingIncentiveVault {
     }
 
     /**
-     * @notice Allows owner to set incentive amounts on a per-token per-market basis
+     * @notice Allows owner to set staking incentive amounts on a per-token per-market basis
      * @param market The predictions market to incentivize
      * @param token The token in which incentives should be denominated
      * @param incentivePerEpoch The maximum number of tokens to give out each epoch
      */
-    function setIncentive(
+    function setStakingIncentive(
         address market,
         address token,
         uint256 incentivePerEpoch
     ) private {
         require(msg.sender == multisig, "Not authorized");
-        incentivesPerEpoch[market][token] = incentivePerEpoch;
+        stakingIncentivesPerEpoch[market][token] = incentivePerEpoch;
     }
 
     /**
-     * @notice Allows a predictions contract to claim incentives on behalf of a user
+     * @notice Allows a predictions contract to claim staking incentives on behalf of a user
      * @dev Should only be called once per proposal. And fails if vault has insufficient
      * funds to make good on incentives
      * @param key The key of the proposal for which incentives are being claimed
@@ -70,7 +73,7 @@ contract StakingIncentiveVault {
      * @param reward The preALOE reward earned by the user
      * @param stakeTotal The total amount of preALOE staked in the pertinent epoch
      */
-    function claimIncentives(
+    function claimStakingIncentives(
         uint40 key,
         address[] calldata tokens,
         address to,
@@ -78,7 +81,7 @@ contract StakingIncentiveVault {
         uint80 stakeTotal
     ) external {
         for (uint256 i = 0; i < tokens.length; i++) {
-            uint256 incentivePerEpoch = incentivesPerEpoch[msg.sender][tokens[i]];
+            uint256 incentivePerEpoch = stakingIncentivesPerEpoch[msg.sender][tokens[i]];
             if (incentivePerEpoch == 0) continue;
 
             if (didClaim(msg.sender, key, tokens[i])) continue;
@@ -89,5 +92,29 @@ contract StakingIncentiveVault {
                 "Failed transfer"
             );
         }
+    }
+
+    /**
+     * @notice Allows owner to set advance incentive amounts on a per-market basis
+     * @param market The predictions market to incentivize
+     * @param token The token in which incentives should be denominated
+     * @param amount The number of tokens to give out on each `advance()`
+     */
+    function setAdvanceIncentive(
+        address market,
+        address token,
+        uint80 amount
+    ) private {
+        require(msg.sender == multisig, "Not authorized");
+        advanceIncentives[market][token] = amount;
+    }
+
+    /**
+     * @notice Allows a predictions contract to claim advance incentives on behalf of a user
+     * @param token The token for which incentive should be claimed
+     * @param to The user to whom incentive should be sent
+     */
+    function claimAdvanceIncentive(address token, address to) external {
+        require(IERC20(token).transfer(to, advanceIncentives[msg.sender][token]), "Failed transfer");
     }
 }
